92e2a06bb940a14b333d1fa4a8e587031872b187
Anastasia Wohlmuth <anastasia.wohlmuth@student.htldornbirn.at>
2024-05-08 11:29:12 +0200

    a

---
+++ src/org/newdawn/slick/Homescreen.java
@@ -0,0 +1,26 @@
+package org.newdawn.slick;
+
+public class Homescreen {
+<!DOCTYPE html>
+<html lang="de">
+<head>
+   <meta charset="utf-8">
+    <title>Our_little_Game</title>
+
+   <style type="text/css">
+            …
+   </style>
+   <script src="script.js" defer></script>
+</head>
+<body>
+<a href="https://www.example.com">
+    <button>Game Start</button>
+
+</a>
+    <a href="https://www.example.com">
+    <button>Game End</button>
+
+</a>
+</body>
+
+}
